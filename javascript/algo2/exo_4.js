39f5584300a5f771b443cd92dbfe2ae17ada3a38
Romain Demaiz <vipdemaiz@gmail.com>
2025-11-13 11:12:30 +0100

    [NEW] trying exo 5

--- javascript/algo2/exo_4.js
+++ javascript/algo2/exo_4.js
@@ -9,7 +9,6 @@ for (let i = 0; i < size; i++) {
     let row = ""
     if(squarePattern == true) {
         for (let j = 0; j < size; j++) {
-        
             if (i === 0 || i === size - 1 || j === 0 || j === size - 1) {
                 row += black
             } else if ( i === 1 || i === size - 2 || j === 1 || j === size -2 ) {
--- javascript/algo2/exo_5.js
+++ javascript/algo2/exo_5.js
@@ -3,16 +3,21 @@ let black = "⬛"
 let white = "⬜"
 
 let size = 25
+let n = 2
 
 for (let i = 0; i < size; i++) {
     let row = ""
 
     for (let j = 0; j < size; j++) {
-        if (i === 0 || i === size - 1 || j === 0 || j === size - 1) {
+    
+        if (i === 0 || i === size - 1 || j === 0 || j === size - 1 ) {
             row += black
-        } else {
+        } else if ( i === n - 1|| i === size - n || j === n - 1 || j === size - n ) {
             row += white
+        } else {
+            row += black
         }
+        
     }
     console.log(row)
 }
