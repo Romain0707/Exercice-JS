e37880d6095772eac702befb794c48588499dbea
Romain Demaiz <vipdemaiz@gmail.com>
2025-11-14 15:57:19 +0100

    [NEW] added exercice 13

---
+++ javascript/Arrays/exo_13.js
@@ -0,0 +1,10 @@
+let sentance = "Bonjour je m'appel Romain, j'aime programmé"
+
+
+function truncate(str, maxlength) {
+  return (str.length > maxlength) ?
+    str.slice(0, maxlength - 1) + '…' : str;
+}
+
+
+console.log(truncate(sentance, 20))
