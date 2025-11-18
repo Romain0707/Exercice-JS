e4a9e10adc7f47213c88ae9c1e79837d80c3f299
Romain0707 <vipdemaiz@gmail.com>
2025-11-18 23:34:28 +0100

    Refactor thumbnail click event listeners
    Refactored event listeners for thumbnail images to use Array.from and forEach for better scalability.

--- javascript/DOM/exo_3/script.js
+++ javascript/DOM/exo_3/script.js
@@ -1,26 +1,8 @@
-const miniImg = document.getElementsByClassName('thumbnail')
-const mainImg = document.getElementById('mainImage')
+const miniImg = document.getElementsByClassName('thumbnail');
+const mainImg = document.getElementById('mainImage');
 
-
-console.log(miniImg)
-
-
-miniImg[0].addEventListener('click', function() {
-    const link = miniImg[0].getAttribute('src')
-    mainImg.setAttribute('src', link)
-})
-
-miniImg[1].addEventListener('click', function() {
-    const link = miniImg[1].getAttribute('src')
-    mainImg.setAttribute('src', link)
-})
-
-miniImg[2].addEventListener('click', function() {
-    const link = miniImg[2].getAttribute('src')
-    mainImg.setAttribute('src', link)
-})
-
-miniImg[3].addEventListener('click', function() {
-    const link = miniImg[3].getAttribute('src')
-    mainImg.setAttribute('src', link)
-})
+Array.from(miniImg).forEach(img => {
+    img.addEventListener('click', () => {
+        mainImg.src = img.src;
+    });
+});
