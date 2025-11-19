8f20572e360ecf271147e45c6d0545c214b85e8f
Romain Demaiz <vipdemaiz@gmail.com>
2025-11-19 13:36:25 +0100

    [NEW] Added 0 before minutes and hours under 10 to get thing prettier

--- javascript/interval/exo_4/script.js
+++ javascript/interval/exo_4/script.js
@@ -3,7 +3,13 @@ const clock = document.getElementById('clock')
 function clocked() {
     const now = new Date()
     let hours = now.getHours()
+    if(hours < 10) {
+        hours ='0' + now.getHours()
+    }
     let minutes = now.getMinutes()
+    if(minutes < 10) {
+        minutes ='0' + now.getMinutes()
+    }
     let seconds = now.getSeconds()
     if(seconds < 10) {
         seconds ='0' + now.getSeconds()
